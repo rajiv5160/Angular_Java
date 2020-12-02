ce2c21358c03b1e09838496f89309f78be2a0b12
Rajiv <rajivedulapram610@gmail.com>
2020-12-01 18:27:40 -0800

    Angular in client and Java in demo

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CarListComponent } from './car-list/car-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CarListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
